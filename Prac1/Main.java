fd25a86b23c49252f0cef32f09f3390246c8f15f
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-02-28 21:42:46 +0200

    Added Test Case
    Added a test for the constructor of the node class

--- Prac1/Main.java
+++ Prac1/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    
     public static void main(String[] args) {
         /*SkipList<Integer> myList = new SkipList<>(3);
         for (int i = 0; i < 10; i++) {
@@ -10,9 +11,13 @@ public class Main {
         myList.printSearchPath(8);
         System.out.print("Searching for 2\t");
         myList.printSearchPath(2);*/
+
+        testSLNConst();
     }
 
-    void testSLNConst(){
+    static void testSLNConst(){
+        SkipListNode<Integer> sln = new SkipListNode<Integer>(2, 1);
+        System.out.println(sln.emptyString());
 
     }
 }
