8faa9b5c77aa57d8a39e1bd21829414df227288f
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-03-09 21:57:49 +0200

    Implemented NaturalOrderList

--- Prac2/NaturalOrderList.java
+++ Prac2/NaturalOrderList.java
@@ -1,16 +1,41 @@
 public class NaturalOrderList<T extends Comparable<T>> extends SelfOrderingList<T> {
     @Override
     public SelfOrderingList<T> getBlankList() {
-        //TODO: Implement the function
+        NaturalOrderList<T> ret = new NaturalOrderList<T>();
+        return ret;
     }
-
+    
     @Override
     public void access(T data) {
-        
+        return;
     }
-
+    
     @Override
     public void insert(T data) {
-        //TODO: Implement the function
+        Node<T> newNode = new Node<T>(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node<T> prevNode = null;
+        Node<T> currNode = head;
+        while (currNode != null && data.compareTo(currNode.data) <= 0) {
+            prevNode = currNode;
+            currNode = currNode.next;
+        }
+        if (prevNode == null) {
+            head = newNode;
+            newNode.next = currNode;
+            currNode.prev = newNode;
+            return;
+        } else if (currNode == null) {
+            prevNode.next = newNode;
+            newNode.prev = prevNode;
+            return;
+        }
+        prevNode.next = newNode;
+        currNode.prev = newNode;
+        newNode.prev = prevNode;
+        newNode.next = currNode;
     }
 }
