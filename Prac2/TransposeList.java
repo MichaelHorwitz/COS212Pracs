0b7ea030363bcc1269b7fa892f53ca7ef19af7d2
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-03-09 21:58:31 +0200

    Started TransposeList implementation

--- Prac2/TransposeList.java
+++ Prac2/TransposeList.java
@@ -1,11 +1,39 @@
 public class TransposeList<T extends Comparable<T>> extends SelfOrderingList<T> {
     @Override
     public SelfOrderingList<T> getBlankList() {
-        //TODO: Implement the function
+        TransposeList<T> ret = new TransposeList<T>();
+        return ret;
     }
-
+    
     @Override
     public void access(T data) {
-        //TODO: Implement the function
+        if (head == null) {
+            return;
+        }
+    
+        Node<T> currNode = head;
+        Node<T> prevNode = null;
+        boolean found = false;
+    
+        while (currNode != null && !found) {
+            if (currNode.data == data) {
+                found = true;
+            } else {
+                prevNode = currNode;
+                currNode = currNode.next;
+            }
+        }
+        
+        if (!found || prevNode == null) {
+            return;
+        }
+
+        prevNode.prev.next = currNode;
+        currNode.next.prev = prevNode;
+        currNode.prev = prevNode.prev;
+        prevNode.next = currNode.next;
+        currNode.next = prevNode;
+        prevNode.prev = currNode;
+
     }
 }
