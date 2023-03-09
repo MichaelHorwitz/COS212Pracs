12fcd12b1ed1b5c8f6c4f414d322180a67af9747
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-03-09 21:57:07 +0200

    Add test cases for finished classes

--- Prac2/App.java
+++ Prac2/App.java
@@ -1,6 +1,69 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        //TODO: Implement an extensive testing main!!!
+        //testCountListBlank();
+        //testCLAccess();
+        //testMTFAccess();
+        testNOAccess();
+        
     }
-
+    static void testNOAccess(){
+        NaturalOrderList<Integer> no = new NaturalOrderList<Integer>();
+        no.insert(4);
+        System.out.println(no);
+        System.out.println("**********************************");
+        no.insert(2);
+        System.out.println(no);
+        System.out.println("**********************************");
+        no.insert(3);
+        System.out.println(no);
+        System.out.println("**********************************");
+        no.insert(3);
+        System.out.println(no);
+        System.out.println("**********************************");
+        no.insert(1);
+        System.out.println(no);
+        System.out.println("**********************************");
+    }
+    static void testCountListBlank(){
+        CountList<Integer> cl = new CountList<Integer>();
+        cl.insert(1);
+        cl.insert(2);
+        cl.insert(3);
+        cl.insert(4);
+        System.out.println(cl);
+    }
+    static void testCLAccess(){
+        CountList<Integer> cl = new CountList<Integer>();
+        cl.insert(0);
+        cl.insert(1);
+        cl.insert(2);
+        cl.insert(3);
+        cl.insert(4);
+        System.out.println(cl); 
+        cl.access(2);    
+        System.out.println(cl);
+        cl.access(4);    
+        System.out.println(cl);
+        cl.access(2);    
+        System.out.println(cl);
+        cl.access(1);    
+        System.out.println(cl);
+    }
+    static void testMTFAccess(){
+        MoveToFrontList<Integer> mtf = new MoveToFrontList<Integer>();
+        mtf.insert(0);
+        mtf.insert(1);
+        mtf.insert(2);
+        mtf.insert(3);
+        mtf.insert(4);
+        System.out.println(mtf); System.out.println("*********************");
+        mtf.access(2);    
+        System.out.println(mtf); System.out.println("*********************");
+        mtf.access(4);    
+        System.out.println(mtf); System.out.println("*********************");
+        mtf.access(2);    
+        System.out.println(mtf); System.out.println("*********************");
+        mtf.access(1);    
+        System.out.println(mtf); System.out.println("*********************");
+    }
 }
