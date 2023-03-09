f6b308a5701eeebc435fea90a05a2f38092640c4
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-03-09 21:57:28 +0200

    Added file

--- Prac2/IterativeTraverse.java
+++ Prac2/IterativeTraverse.java
@@ -1,3 +1,5 @@
+/* 
+
 public class IterativeTraverse<T extends Comparable<T>> extends Traverser<T>{
     public IterativeTraverse(){
         //TODO: Implement the function
@@ -6,39 +8,40 @@ public class IterativeTraverse<T extends Comparable<T>> extends Traverser<T>{
     public IterativeTraverse(SelfOrderingList<T> list){
         //TODO: Implement the function
     }
-
+    
     @Override
     public SelfOrderingList<T> reverseList() {
         //TODO: Implement the function
     }
-
+    
     @Override
     public boolean contains(T data) {
         //TODO: Implement the function
     }
-
+    
     @Override
     public String toString() {
         //TODO: Implement the function
     }
-
+    
     @Override
     public Node<T> get(int pos) {
         //TODO: Implement the function
     }
-
+    
     @Override
     public Node<T> find(T data) {
         //TODO: Implement the function
     }
-
+    
     @Override
     public int size() {
         //TODO: Implement the function
     }
-
+    
     @Override
     public SelfOrderingList<T> clone(SelfOrderingList<T> otherList) {
         //TODO: Implement the function
     }
 }
+*/
