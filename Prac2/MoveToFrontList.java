9259ace7716f1c38669f5489c2e786b14417f61b
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-03-09 21:57:39 +0200

    Implemented MoveToFrontList

--- Prac2/MoveToFrontList.java
+++ Prac2/MoveToFrontList.java
@@ -1,11 +1,45 @@
 public class MoveToFrontList<T extends Comparable<T>> extends SelfOrderingList<T> {
     @Override
     public SelfOrderingList<T> getBlankList() {
-        //TODO: Implement the function
+        MoveToFrontList<T> ret = new MoveToFrontList<>();
+        return ret;
     }
-
+    
     @Override
     public void access(T data) {
-        //TODO: Implement the function
+        if (head == null) {
+            return;
+        }
+        Node<T> currNode = head;
+        boolean found = false;
+        while (currNode != null && !found) {
+            if (currNode.data == data) {
+                found = true;
+            } else {
+                currNode = currNode.next;
+            }
+        }
+        if (!found) {
+            return;
+        }
+        if (currNode.prev == null && currNode.next == null) {
+            return;
+        } else if (currNode.prev == null){
+            currNode.next.prev = currNode.prev;
+            head = currNode;
+        } else if (currNode.next == null){
+            currNode.prev.next = currNode.next;
+        } else {
+            currNode.prev.next = currNode.next;
+            currNode.next.prev = currNode.prev;
+        }
+        Node<T> newNode = currNode;
+        newNode.next = null;
+        newNode.prev = null;
+        currNode = head;
+
+        head = newNode;
+        newNode.next = currNode;
+        currNode.prev = newNode;
     }
 }
