7375a64be122e8c3e8198cab2c5634a6609a0e6a
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-03-10 19:49:44 +0200

    Fixed

--- Prac2/CountList.java
+++ Prac2/CountList.java
@@ -26,8 +26,7 @@ public class CountList<T extends Comparable<T>> extends SelfOrderingList<T>{
         if (currNode.prev == null && currNode.next == null) {
             return;
         } else if (currNode.prev == null){
-            currNode.next.prev = null;
-            head = currNode.next;
+            return;
         } else if (currNode.next == null){
             currNode.prev.next = currNode.next;
         } else {
@@ -38,7 +37,29 @@ public class CountList<T extends Comparable<T>> extends SelfOrderingList<T>{
         newNode.next = null;
         newNode.prev = null;
         currNode = head;
-        while (currNode.next != null && currNode.next.accessCount >= newNode.accessCount) {
+        Node<T> prevNode = null;
+        while (currNode != null && newNode.accessCount <= currNode.accessCount) {
+            prevNode = currNode;
+            currNode = currNode.next;
+        }
+        if (prevNode == null) {
+            head = newNode;
+            newNode.next = currNode;
+            currNode.prev = newNode;
+            return;
+        }
+        if (currNode == null) {
+            prevNode.next = newNode;
+            newNode.prev = prevNode;
+            return;
+        }
+        prevNode.next = newNode;
+        currNode.prev = newNode;
+        newNode.prev = prevNode;
+        newNode.next = currNode;
+
+        /*
+        while (currNode != null && currNode.accessCount >= newNode.accessCount) {
             currNode = currNode.next;
         }
         if (currNode == head) {
@@ -56,5 +77,6 @@ public class CountList<T extends Comparable<T>> extends SelfOrderingList<T>{
         newNode.next = currNode.next;
         currNode.next = newNode;
         newNode.next.prev = newNode;
+        */
     }
 }
