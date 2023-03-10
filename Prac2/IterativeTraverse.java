60eee0900eaa3c7f8a96ab97afb8f2b73304ae7c
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-03-10 09:26:33 +0200

    Started Implementation

--- Prac2/IterativeTraverse.java
+++ Prac2/IterativeTraverse.java
@@ -1,47 +1,67 @@
-/* 
-
-public class IterativeTraverse<T extends Comparable<T>> extends Traverser<T>{
-    public IterativeTraverse(){
-        //TODO: Implement the function
+public class IterativeTraverse<T extends Comparable<T>> extends Traverser<T> {
+    public IterativeTraverse() {
+        list = null;
     };
-    
-    public IterativeTraverse(SelfOrderingList<T> list){
-        //TODO: Implement the function
+
+    public IterativeTraverse(SelfOrderingList<T> list) {
+        if (list == null) {
+            this.list = null;
+            return;
+        }
+        this.list = list.getBlankList();
+        if (list.head == null) {
+            this.list.head = null;
+            return;
+        }
+        Node<T> currNode = list.head;
+        while (currNode != null) {
+            this.list.insert(currNode.data);
+            currNode = currNode.next;
+        }
     }
-    
+
     @Override
     public SelfOrderingList<T> reverseList() {
-        //TODO: Implement the function
+        return null;
     }
     
     @Override
     public boolean contains(T data) {
-        //TODO: Implement the function
+        return false;
     }
     
     @Override
     public String toString() {
-        //TODO: Implement the function
+        if (list.head == null) {
+            return "";
+        }
+        Node<T> currNode = list.head;
+        String ret = "";
+        
+        while (currNode != null) {
+            ret += "->" + currNode;
+            currNode = currNode.next;
+        }
+        return ret;
     }
     
     @Override
     public Node<T> get(int pos) {
-        //TODO: Implement the function
+        return null;
     }
     
     @Override
     public Node<T> find(T data) {
-        //TODO: Implement the function
+        return null;
     }
     
     @Override
     public int size() {
-        //TODO: Implement the function
+        return -1;
     }
-    
+
     @Override
     public SelfOrderingList<T> clone(SelfOrderingList<T> otherList) {
-        //TODO: Implement the function
+        return null;
     }
 }
-*/
