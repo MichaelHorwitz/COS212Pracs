f8029ea34cdae3420a1c8b0778adb06c5ea37ab9
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-03-10 11:21:48 +0200

    Implemented Iterative

--- Prac2/IterativeTraverse.java
+++ Prac2/IterativeTraverse.java
@@ -22,11 +22,35 @@ public class IterativeTraverse<T extends Comparable<T>> extends Traverser<T> {
 
     @Override
     public SelfOrderingList<T> reverseList() {
-        return null;
+        if (list == null) {
+            return null;
+        }
+
+        SelfOrderingList<T> newList = list.getBlankList();
+        if (list.head == null) {
+            return newList;
+        }
+        Node<T> currNode = list.head;
+
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        while (currNode != null) {
+            newList.insert(currNode.data);
+            currNode = currNode.prev;
+        }
+        return newList;
     }
     
     @Override
     public boolean contains(T data) {
+        Node<T> currNode = list.head;
+        while (currNode!= null) {
+            if (currNode.data.compareTo(data) == 0) {
+                return true;
+            }
+            currNode = currNode.next;            
+        }
         return false;
     }
     
@@ -47,21 +71,61 @@ public class IterativeTraverse<T extends Comparable<T>> extends Traverser<T> {
     
     @Override
     public Node<T> get(int pos) {
+        if (list == null) {
+            return null;
+        }
+        if (list.head == null) {
+            return null;
+        }
+        int i = 0;
+        Node<T> currNode = list.head;
+        while (i < pos && currNode != null) {
+            currNode = currNode.next;
+            i++;
+        }
+        if (pos == i) {
+            return currNode;
+        }
         return null;
     }
     
     @Override
     public Node<T> find(T data) {
+        Node<T> currNode = list.head;
+        while (currNode!= null) {
+            if (currNode.data.compareTo(data) == 0) {
+                return currNode;
+            }
+            currNode = currNode.next;            
+        }
         return null;
     }
     
     @Override
     public int size() {
-        return -1;
+        if(list.head == null){
+            return 0;
+        }
+        Node<T> currNode = list.head;
+        int size = 0;
+        while (currNode != null) {
+            size++;
+            currNode = currNode.next;
+        }
+        return size;
     }
 
     @Override
     public SelfOrderingList<T> clone(SelfOrderingList<T> otherList) {
-        return null;
+        SelfOrderingList<T> newList = otherList.getBlankList();
+        if (otherList.head == null) {
+            return newList;
+        }
+        Node<T> currNode = otherList.head;
+        while (currNode != null) {
+            newList.insert(currNode.data);
+            currNode = currNode.next;
+        }
+        return newList;
     }
 }
