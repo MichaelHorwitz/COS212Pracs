d42220961c2b3e9f0139ffe1463aaa6498e755d0
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-03-10 11:21:30 +0200

    Added test for iterative

--- Prac2/App.java
+++ Prac2/App.java
@@ -5,10 +5,41 @@ public class App {
         //testMTFAccess();
         //testNOAccess();
         //testTLAccess();
-        testITstr();
+        //testITstr();
+        testIT();
         
     }
     
+    static void testIT(){
+        CountList<Integer> cl = new CountList<Integer>();
+        cl.insert(0);
+        cl.insert(1);
+        cl.insert(2);
+        cl.insert(3);
+        cl.insert(4);
+        IterativeTraverse<Integer> it = new IterativeTraverse<Integer>(cl);
+        System.out.println(it);
+        System.out.println("Contains 0: " + it.contains(0));
+        System.out.println("Contains 2: " + it.contains(2));
+        System.out.println("Contains 4: " + it.contains(4));
+        System.out.println("Contains 8: " + it.contains(8));
+
+        System.out.println("Size: " + it.size());
+        System.out.println("Reversed:\n" + it.reverseList());
+
+        System.out.println("Find 0: " + it.find(0));
+        System.out.println("Find 2: " + it.find(2));
+        System.out.println("Find 4: " + it.find(4));
+        System.out.println("Find 8: " + it.find(8));
+
+        System.out.println("Get 0: " + it.get(0));
+        System.out.println("Get 2: " + it.get(2));
+        System.out.println("Get 4: " + it.get(4));
+        System.out.println("Get 8: " + it.get(8));
+
+        System.out.println(it.clone(cl));
+    }
+
     static void testITstr(){
         CountList<Integer> cl = new CountList<Integer>();
         IterativeTraverse it = new IterativeTraverse<>(cl);
