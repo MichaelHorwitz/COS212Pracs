3c2a47aa790225a2b1146ef251cd7f0e77d57c35
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-05-10 11:55:19 +0200

    small start

--- Prac6/Graph.java
+++ Prac6/Graph.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Graph {
@@ -9,9 +10,40 @@ public class Graph {
     private int numEdges;
 
     public Graph(String fileName) {
+        if (fileName.equals("")) {
+            vertices = new String[0];
+            adjacencyMatrix = new Integer[0][0];
+            numVertices = 0;
+            numEdges = 0;
+            return;
+        }
+        Scanner sc;
+        sc = new Scanner(fileName);
+        numVertices = sc.nextInt();
+        String currLine = sc.nextLine();
+        vertices = currLine.split(" ");
+        adjacencyMatrix = new Integer[numVertices][numVertices];
+        int lineNum = 0;
+        while (sc.hasNextLine()) {
+            currLine = sc.nextLine();
+            String [] currLineArr = currLine.split(" ");
+            for (int i = 0; i < currLineArr.length; i++) {
+                adjacencyMatrix[lineNum][i] = Integer.valueOf(currLineArr[i]);
+            }
+            lineNum++;
+        }
+
     }
 
     public void insertVertex(String name) {
+        String[] newVertices = new String[numVertices + 1];
+        newVertices[numVertices] = name;
+        for (int i = 0; i < numVertices; i++) {
+            newVertices[i] = vertices[i];
+        }
+        vertices[numVertices] = name;
+        
+        numVertices++;
     }
 
     public void insertEdge(String start, String end, int weight) {
