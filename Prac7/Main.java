befb9714bf605903fa1c307e092d5a66e8651792
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-05-19 19:59:07 +0200

    More minor debugging

--- Prac7/Main.java
+++ Prac7/Main.java
@@ -2,7 +2,84 @@ public class Main {
     public static void main(String[] args) {
         //givenMain();
         //test1();
-        test2();
+        //test2();
+        //test3();
+        test4();
+    }
+    private static void test4(){
+        Graph g = new Graph();
+
+        String[] verts = { "a", "b", "c", "d", "e", "f", "g", "h" };
+        String[][] edges = { { "a", "e", "1" },
+                { "a", "f", "-1" },
+                { "a", "g", "2" },
+                { "b", "e", "-2" },
+                { "b", "c", "3" },
+                { "b", "h", "-3" },
+                { "c", "g", "4" },
+                { "d", "f", "-4" },
+                { "d", "g", "5" },
+                { "f", "g", "-5" },
+                { "f", "h", "6" },
+                { "g", "h", "-6" } };
+
+        for (String s : verts) {
+            g.addVertex(s);
+        }
+
+        for (String[] e : edges) {
+            g.addEdge(e[0], e[1], Integer.valueOf(e[2]));
+        }
+        String[][] rem = {
+            {"a", "b"},
+            {"c", "b"},
+            {"f", "g"},
+            {"#", "%"}
+        };
+        System.out.println(g);
+        for (String[] r : rem) {
+            g.removeEdge(r[0], r[1]);
+            System.out.println("###############################################\n" + g);
+            System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+            for (Edge e : g.edges) {
+                System.out.println(e);
+            }
+        }
+        Graph g2 = new Graph();
+        g2.removeEdge("a", "b");
+        System.out.println(g2);
+    }
+    private static void test3(){
+        Graph g = new Graph();
+        String[] verts = {"B", "A", "C", "D"};
+        for (String v : verts) {
+            g.addVertex(v);
+        }
+        String[][] edges = { 
+            { "A", "B", "2"  },
+            { "B", "C", "-7" },
+            { "A", "D", "200" },
+            { "C", "D", "-5" }
+
+        };
+        for (String[] e : edges) {
+            g.addEdge(e[0], e[1], Integer.valueOf(e[2]));
+        }
+        String[][] rem = {
+            {"A", "D"},
+            {"B", "C"},
+            {"A", "B"},
+            {"A", "D"},
+            {"C", "D"}
+        };
+        System.out.println(g);
+        for (String[] r : rem) {
+            g.removeEdge(r[0], r[1]);
+            System.out.println("#######################################################");
+            for (Edge e : g.edges) {
+                System.out.println(e);
+            }
+        }
     }
     private static void test2(){
         Graph g = new Graph();
@@ -38,12 +115,12 @@ public class Main {
         for (String v : verts) {
             g.addVertex(v);
         }
-        System.out.println(g);
+        //System.out.println(g);
         String[] rem = {"C", "A", "E", "B", "D"};
         for (String v : rem) {
             g.removeVertex(v);
-            System.out.println("###########################################");
-            System.out.println(g);
+            //System.out.println("###########################################");
+            //System.out.println(g);
         }
         for (String v : verts) {
             g.addVertex(v);
@@ -54,11 +131,27 @@ public class Main {
         { "C", "A", "-2" },
         { "D", "A", "-3" },
         { "C", "D", "4" },
+        { "A", "A", "4" },
         { "D", "B", "-7" } };
         for (String[] e : edges) {
             g.addEdge(e[0], e[1], Integer.valueOf(e[2]));
+            //System.out.println(g);
         }
         System.out.println(g);
+        String[][] remE = { 
+            { "D", "A", "-3" },
+            { "C", "D", "4" },
+            { "B", "C", "5" },
+            { "B", "A", "2"  },
+            { "C", "A", "-2" },
+            { "A", "A", "-2" },
+            { "D", "B", "-7" } };
+        System.out.println("REMOVING NOW *************************");
+        for (String[] e : remE) {
+                g.removeEdge(e[0], e[1]);
+                System.out.println("###########################################");
+                System.out.println(g);
+            }
     }
     private static void givenMain(){
         Graph g = new Graph();
