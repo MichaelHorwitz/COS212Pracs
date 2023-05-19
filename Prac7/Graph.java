5c7879e635fb6ca34769929e6a9cd93b72e854fd
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-05-19 16:25:52 +0200

    Update Graph.java

--- Prac7/Graph.java
+++ Prac7/Graph.java
@@ -168,15 +168,197 @@ public class Graph {
     }
 
     public Graph minSpanningTree() {
-        return null;
+        Graph ret = new Graph();
+        Edge[] newEdges = new Edge[edges.length];
+        Vertex[] newVertices = new Vertex[vertices.length];
+        for (int i = 0; i < newVertices.length; i++) {
+            newVertices[i] = new Vertex(vertices[i].name);
+            ret.addVertex(vertices[i].name);
+        }
+        for (int i = 0; i < newEdges.length; i++) {
+            int a = findVertIndex(edges[i].vertexA);
+            int b = findVertIndex(edges[i].vertexB);
+            newEdges[i] = new Edge(newVertices[a], newVertices[b], edges[i].weight);
+        }
+        boolean sorted = false;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < newEdges.length - 1; i++) {
+                if (newEdges[i].weight > newEdges[i + 1].weight) {
+                    sorted = false;
+                    Edge temp = newEdges[i];
+                    newEdges[i] = newEdges[i+1];
+                    newEdges[i+1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < edges.length && ret.vertices.length < vertices.length; i++) {
+            ret.addEdge(edges[i].vertexA.name, edges[i].vertexB.name, edges[i].weight);
+            if (ret.cycle()) {
+                ret.removeEdge(edges[i].vertexA.name, edges[i].vertexB.name);
+            }
+        }
+        return ret;
     }
 
     public Vertex[][] brelazColouring() {
-        return null;
+        int[] saturationDeg = new int[vertices.length];
+        int[] uncoloredDeg = new int[vertices.length];
+        int[] deg = getDegreeAll();
+        int[] color = new int[vertices.length];
+        int maxCol = -1;
+        for (int i = 0; i < vertices.length; i++) {
+            saturationDeg[i] = 0;
+            uncoloredDeg[i] = deg[i];
+            color[i] = -1;
+        }
+        while (hasZero(color)) {
+            int v = findBCAV(saturationDeg, uncoloredDeg, color); //find max uncolored with highest saturation
+            int j = findBCAJ(v, color);
+            int [] neighbours = neighbours(v);
+            for (int i = 0; i < neighbours.length; i++) {
+                if (color[neighbours[i]] == -1) {
+                    int u = neighbours[i];
+                    int [] uNeigh = neighbours(u);
+                    boolean assignedCj = false;
+                    for (int k = 0; k < uNeigh.length; k++) {
+                        if (color[uNeigh[k]] == j) {
+                            assignedCj = true;
+                        }
+                    }
+                    if (!assignedCj) {
+                        saturationDeg[u]++;
+                    }
+                    uncoloredDeg[u]--;
+                }
+            }
+            if(j > maxCol){
+                maxCol = j;
+            }
+            color[v] = j;
+        }
+        Vertex[][] tempArr = new Vertex[maxCol + 1][vertices.length];
+        int[] numVertices = new int[maxCol + 1];
+        for (int i = 0; i < numVertices.length; i++) {
+            numVertices[i] = 0;
+        }
+        for (int i = 0; i < color.length; i++) {
+            int currCol = color[i];
+            int currNum = numVertices[currCol]++;
+            tempArr[currCol][currNum] = vertices[i];
+        }
+        Vertex[][] ret = new Vertex[maxCol + 1][];
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = new Vertex[numVertices[i]];
+            for (int j = 0; j < numVertices[i]; j++) {
+                ret[i][j] = tempArr[i][j];
+            }
+        }
+        return ret;
     }
-
+    private int findBCAJ(int v, int[] color){
+        int j = 0;
+        int [] neighbours = neighbours(v);
+        boolean correctVal = false;
+        while (!correctVal) {
+            correctVal = true;
+            for (int i = 0; i < neighbours.length; i++) {
+                if (j == color[i]) {
+                    j++;
+                    correctVal = false;
+                }
+            }
+        }
+        return j;
+        
+    }
+    private int[] neighbours(int index){
+        int[] ret = new int[vertices.length];
+        int numNeigh = 0;
+        for (int i = 0; i < edges.length; i++) {
+            if (edges[i].vertexA.compareTo(vertices[index]) == 0) {
+                ret[numNeigh] = findVertIndex(edges[i].vertexA);
+                numNeigh++;
+            }
+            if (edges[i].vertexB.compareTo(vertices[index]) == 0) {
+                ret[numNeigh] = findVertIndex(edges[i].vertexB);
+                numNeigh++;
+            }
+        }
+        int[] tempArr = new int[numNeigh];
+        for (int i = 0; i < tempArr.length; i++) {
+            tempArr[i] = ret[i];
+        }
+        ret = tempArr;
+        return ret;
+    }
+    private int findBCAV(int [] saturationDeg, int [] uncoloredDeg, int [] color){
+        int index = -1;
+        int maxUnCol = -1;
+        int maxSat = -1;
+        for (int i = 0; i < color.length; i++) {
+            if (color[i] != -1) {
+                if (saturationDeg[i] > maxSat) {
+                    index = i;
+                    maxSat = saturationDeg[i];
+                    maxUnCol = uncoloredDeg[i];
+                }
+                if (saturationDeg[i] == maxSat) {
+                    if (uncoloredDeg[i] > maxUnCol) {
+                        index = i;
+                        maxSat = saturationDeg[i];
+                        maxUnCol = uncoloredDeg[i];
+                    }
+                }
+            }
+        }
+        return index;
+    }
+    private boolean hasZero(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private int getDegree(int index){
+        int deg = 0;
+        for (int i = 0; i < edges.length; i++) {
+            if (edges[i].vertexA.compareTo(vertices[index]) == 0 || edges[i].vertexB.compareTo(vertices[index]) == 0) {
+                deg++;
+            }
+        }
+        return deg;
+    }
+    private int[] getDegreeAll(){
+        int[] ret = new int[vertices.length];
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = getDegree(i);
+        }
+        return ret;
+    }
     @Override
     public String toString() {
+        int[][] adjMatrix = new int [vertices.length][vertices.length];
+        for (int i = 0; i < vertices.length; i++) {
+            for (int j = 0; j < vertices.length; j++) {
+                adjMatrix[i][j] = 0;
+            }
+        }
+        for (int i = 0; i < edges.length; i++) {
+            int a = findVertIndex(edges[i].vertexA);
+            int b = findVertIndex(edges[i].vertexB);
+            adjMatrix[a][b] = edges[i].weight;
+            adjMatrix[b][a] = adjMatrix[a][b];
+        }
+        String ret = "";
+        for (int i = 0; i < vertices.length; i++) {
+            for (int j = 0; j < vertices.length; j++) {
+                
+            }
+            ret += "\n";
+        }
         return "";
     }
 }
