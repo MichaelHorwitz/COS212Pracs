a53e448012643fd49ca23321b4b2c5f55a917374
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-05-19 10:27:12 +0200

    Added specification documents

---
+++ Prac7/Graph.java
@@ -0,0 +1,37 @@
+public class Graph {
+    public Vertex[] vertices = new Vertex[0];
+    public Edge[] edges = new Edge[0];
+
+    public void addVertex(String v) {
+    }
+
+    public void removeVertex(String v) {
+    }
+
+    public void addEdge(String a, String b, int w) {
+    }
+
+    public void removeEdge(String a, String b) {
+    }
+
+    public int[][] unionFind() {
+        return null;
+    }
+
+    public boolean cycle() {
+        return false;
+    }
+
+    public Graph minSpanningTree() {
+        return null;
+    }
+
+    public Vertex[][] brelazColouring() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
