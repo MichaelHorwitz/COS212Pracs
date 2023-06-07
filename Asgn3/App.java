c2d9a661516d897ef4bc22f3ef0e1e245d2ecb25
MichaelHorwitz <91372554+MichaelHorwitz@users.noreply.github.com>
2023-06-07 11:56:34 +0200

    Implemented myDS

--- Asgn3/App.java
+++ Asgn3/App.java
@@ -1,9 +1,33 @@
 public class App {
     public static void main(String[] args) {
-        SpecExample1();
+        // SpecExample1();
+        testMyDSInsertRemove();
     }
 
-    public static void SpecExample1(){
+    public static void testMyDSInsertRemove() {
+        myDS<Integer> mds = new myDS<Integer>();
+        Integer[] toAdd = { 2, 3, 5, 4, 7, 1 };
+        System.out.println("TESTING INSERT");
+        for (Integer integer : toAdd) {
+            mds.insert(integer);
+            System.out.println(mds);
+        }
+        System.out.println("TESTING TOARRAY");
+        Comparable<Integer>[] minArr = mds.toArray();
+        for (Comparable<Integer> comparable : minArr) {
+            System.out.print(comparable + ", ");
+        }
+        System.out.println();
+        System.out.println("TESTING REMOVE");
+        Integer[] toRem = {3,2,21,4,7,5,1};
+        for (Integer integer : toRem) {
+            mds.remove(integer);
+            System.out.println(mds);
+        }
+
+    }
+
+    public static void SpecExample1() {
         Node N1 = new Node("N1");
         Node N2 = new Node("N2");
         Node N3 = new Node("N3");
@@ -22,19 +46,19 @@ public class App {
         cfg.addEdge("E5", cfg.getNode("N3"), cfg.getNode("N5"), 0);
         printGraph(cfg);
         Path[] simplePaths = cfg.getSimplePaths();
-        for(Path p: simplePaths){
+        for (Path p : simplePaths) {
             System.out.println(p.toString());
         }
         printBreaker();
         Path[] primePaths = cfg.getPrimePaths();
-        for(Path p: primePaths){
+        for (Path p : primePaths) {
             System.out.println(p.toString());
         }
     }
 
-    public static void printGraph(CFG graph){
+    public static void printGraph(CFG graph) {
         printBreaker();
-        if(graph == null){
+        if (graph == null) {
             System.out.println("NULL Graph");
         } else {
             System.out.println(graph.toString());
@@ -42,49 +66,50 @@ public class App {
         printBreaker();
     }
 
-    public static void printBreaker(){
+    public static void printBreaker() {
         System.out.println("###");
     }
 
-/* Expected output
-###
-N1:
-        -> E1 -[0]-> N2
-N2:
-        -> E2 -[0]-> N3
-N3:
-        -> E3 -[0]-> N4
-        -> E5 -[0]-> N5
-N4:
-        -> E4 -[0]-> N3
-N5:
-        No out going edges
-
-###
-N1
-N2
-N3
-N4
-N5
-N1-> E1 -[0]-> N2
-N2-> E2 -[0]-> N3
-N3-> E3 -[0]-> N4
-N3-> E5 -[0]-> N5
-N4-> E4 -[0]-> N3
-N1-> E1 -[0]-> N2-> E2 -[0]-> N3
-N2-> E2 -[0]-> N3-> E3 -[0]-> N4
-N2-> E2 -[0]-> N3-> E5 -[0]-> N5
-N3-> E3 -[0]-> N4-> E4 -[0]-> N3
-N4-> E4 -[0]-> N3-> E3 -[0]-> N4
-N4-> E4 -[0]-> N3-> E5 -[0]-> N5
-N1-> E1 -[0]-> N2-> E2 -[0]-> N3-> E3 -[0]-> N4
-N1-> E1 -[0]-> N2-> E2 -[0]-> N3-> E5 -[0]-> N5
-###
-N3-> E3 -[0]-> N4-> E4 -[0]-> N3
-N4-> E4 -[0]-> N3-> E3 -[0]-> N4
-N4-> E4 -[0]-> N3-> E5 -[0]-> N5
-N1-> E1 -[0]-> N2-> E2 -[0]-> N3-> E3 -[0]-> N4
-N1-> E1 -[0]-> N2-> E2 -[0]-> N3-> E5 -[0]-> N5
- */
+    /*
+     * Expected output
+     * ###
+     * N1:
+     * -> E1 -[0]-> N2
+     * N2:
+     * -> E2 -[0]-> N3
+     * N3:
+     * -> E3 -[0]-> N4
+     * -> E5 -[0]-> N5
+     * N4:
+     * -> E4 -[0]-> N3
+     * N5:
+     * No out going edges
+     * 
+     * ###
+     * N1
+     * N2
+     * N3
+     * N4
+     * N5
+     * N1-> E1 -[0]-> N2
+     * N2-> E2 -[0]-> N3
+     * N3-> E3 -[0]-> N4
+     * N3-> E5 -[0]-> N5
+     * N4-> E4 -[0]-> N3
+     * N1-> E1 -[0]-> N2-> E2 -[0]-> N3
+     * N2-> E2 -[0]-> N3-> E3 -[0]-> N4
+     * N2-> E2 -[0]-> N3-> E5 -[0]-> N5
+     * N3-> E3 -[0]-> N4-> E4 -[0]-> N3
+     * N4-> E4 -[0]-> N3-> E3 -[0]-> N4
+     * N4-> E4 -[0]-> N3-> E5 -[0]-> N5
+     * N1-> E1 -[0]-> N2-> E2 -[0]-> N3-> E3 -[0]-> N4
+     * N1-> E1 -[0]-> N2-> E2 -[0]-> N3-> E5 -[0]-> N5
+     * ###
+     * N3-> E3 -[0]-> N4-> E4 -[0]-> N3
+     * N4-> E4 -[0]-> N3-> E3 -[0]-> N4
+     * N4-> E4 -[0]-> N3-> E5 -[0]-> N5
+     * N1-> E1 -[0]-> N2-> E2 -[0]-> N3-> E3 -[0]-> N4
+     * N1-> E1 -[0]-> N2-> E2 -[0]-> N3-> E5 -[0]-> N5
+     */
 
 }
--- Asgn3/myDS.java
+++ Asgn3/myDS.java
@@ -7,24 +7,61 @@ public class myDS<T extends Comparable<T>>{
         size = 0;
         numElements = 0;
     }
-    public insert(Comparable<T> data){
+    public void insert(Comparable<T> data){
         if (numElements + 1 >= size) {
-            resize(2);
+            resize(2.0);
         }
         mainArr[numElements] = data;
         numElements++;
 
     }
-    public remove(Comparable<T> data){
-
+    public boolean remove(Comparable<T> data){
+        int index = -1;
+        for (int i = 0; i < numElements && index == -1; i++) {
+            if (data.equals(mainArr[i])) {
+                index = i;
+            }
+        }
+        if (index == -1) {
+            return false;
+        }
+        for (int i = index + 1; i < numElements; i++) {
+            mainArr[i-1] = mainArr[i];
+        }
+        mainArr[numElements] = null;
+        numElements--;
+        if (numElements < size/2) {
+            resize(0.5);
+        }
+        return true;
     }
-    private resize(int resizeFactor){
-        int newSize = size * resizeFactor;
+    private void resize(Double resizeFactor){
+        int newSize = (int)(size * resizeFactor);
+        if (size == 0 && resizeFactor > 1) {
+            newSize = 1;
+            mainArr = new Comparable[1];
+            size = newSize;
+            return;
+        }
         Comparable<T>[] newArr = new Comparable[newSize];
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < numElements; i++) {
             newArr[i] = mainArr[i];
         }
         mainArr = newArr;
         size = newSize;
+    }
+    public Comparable<T>[] toArray(){
+        Comparable<T>[] retArr = new Comparable[numElements];
+        for (int i = 0; i < numElements; i++) {
+            retArr[i] = mainArr[i];
+        }
+        return retArr;
+    }
+    public String toString(){
+        String ret = "";
+        for (int i = 0; i < numElements; i++) {
+            ret += mainArr[i] + ", ";
+        }
+        return ret;
     }
 }
